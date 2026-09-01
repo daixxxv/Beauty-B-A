77d2215183dafd087f3b0e92bcb45ab49ef12cc0
daixxxv <ogadai.dreamer06@icloud.com>
2026-09-01 17:49:14 +0900

    GitHub_Actionsの実装

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,7 +36,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
+Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -80,6 +80,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before(:each, type: :system) do
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  end
 
   config.include FactoryBot::Syntax::Methods
 
