e2afa7a462161c723ffc500c32dd0af672f57581
daixxxv <ogadai.dreamer06@icloud.com>
2026-09-03 03:46:01 +0900

    CloudFlareの導入

--- config/initializers/cloudflare_r2.rb
+++ config/initializers/cloudflare_r2.rb
@@ -1,2 +1,6 @@
-Aws.config[:request_checksum_calculation] = "when_required"
-Aws.config[:response_checksum_validation] = "when_required"
+require "aws-sdk-s3" rescue nil
+
+if defined?(Aws)
+  Aws.config[:request_checksum_calculation] = "when_required"
+  Aws.config[:response_checksum_validation] = "when_required"
+end
