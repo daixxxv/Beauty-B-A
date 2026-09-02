6484df022a8bae2610d73a30555d5dac44a26fd7
daixxxv <ogadai.dreamer06@icloud.com>
2026-09-03 01:44:45 +0900

    Resend導入

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "beauty-b-a.onrender.com", protocol: "https" }
 
   config.action_mailer.delivery_method = :resend
-  
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
